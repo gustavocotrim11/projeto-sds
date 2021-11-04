aee040d4bd6ff1fd805397d54a1250a44e699150
Gustavo Cotrim <gustavocotrim11@gmail.com>
2021-11-04 11:13:23 -0300

    Group by search

--- backend/src/main/java/com/devsuperior/dsvendas/services/SellerService.java
+++ backend/src/main/java/com/devsuperior/dsvendas/services/SellerService.java
@@ -5,6 +5,7 @@ import com.devsuperior.dsvendas.entities.Seller;
 import com.devsuperior.dsvendas.repositories.SellerRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 import java.util.stream.Collectors;
@@ -15,6 +16,7 @@ public class SellerService {
     @Autowired
     private SellerRepository repository;
 
+    @Transactional(readOnly = true)
     public List<SellerDTO> findAll() {
         List<Seller> result = repository.findAll();
         return result.stream().map(x -> new SellerDTO(x)).collect(Collectors.toList());
